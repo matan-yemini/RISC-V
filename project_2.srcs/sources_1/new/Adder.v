512df5f28a4ccb81e96b91ed2caad44389fe30d1
matan-yemini <matany55@gmail.com>
2025-02-26 00:29:42 +0800

    Update Adder.v comments

--- project_2.srcs/sources_1/new/Adder.v
+++ project_2.srcs/sources_1/new/Adder.v
@@ -1,12 +1,11 @@
 `timescale 1ns/1ps
 
 module Adder (
-    input [31:0] PCF,              // כתובת ה-PC הנוכחית
-    input [31:0] ImmExtD,             // ImmExtDediate value (Offset)
-    output [31:0] branch_jump_targetD   // כתובת יעד הקפיצה
+    input [31:0] PCF,              
+    input [31:0] ImmExtD,          
+    output [31:0] branch_jump_targetD  
 );
 
-    // Adder ל-PC + ImmExtD
     assign branch_jump_targetD = PCF + ImmExtD;
 
 endmodule
