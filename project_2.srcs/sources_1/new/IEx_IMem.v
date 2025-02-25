1455df21280f002972bfd48fb07d387296572630
matan-yemini <matany55@gmail.com>
2025-02-26 00:32:04 +0800

    Update IEx_IMem.v comments

--- project_2.srcs/sources_1/new/IEx_IMem.v
+++ project_2.srcs/sources_1/new/IEx_IMem.v
@@ -7,13 +7,13 @@ module IEx_IMem (
     input [31:0] pc_plus4E,
     input [1:0] ResultSrcE,
     input MemReadE, MemWriteE,
-    input RegWriteE,             // Input RegWrite signal from Execute stage
+    input RegWriteE,
     output reg [31:0] ALUResultM, WriteDataM,
     output reg [4:0] RdM,
     output reg [31:0] pc_plus4M,
-    output reg [1:0] ResultSrcM,  // 2-bit signal, corrected in reset block
+    output reg [1:0] ResultSrcM,
     output reg MemReadM, MemWriteM,
-    output reg RegWriteM         // Output RegWrite signal to Memory stage
+    output reg RegWriteM
 );
 
     always @(posedge clk or posedge reset) begin
@@ -22,10 +22,10 @@ module IEx_IMem (
             WriteDataM <= 32'b0;
             RdM <= 5'b0;
             pc_plus4M <= 32'b0;
-            ResultSrcM <= 2'b0;  // Corrected from 3'b0 to 2'b0
+            ResultSrcM <= 2'b0;
             MemReadM <= 1'b0;
             MemWriteM <= 1'b0;
-            RegWriteM <= 1'b0;   // Reset RegWrite signal
+            RegWriteM <= 1'b0;
         end else begin
             ALUResultM <= ALUResultE;
             WriteDataM <= RD2E;
@@ -34,7 +34,7 @@ module IEx_IMem (
             ResultSrcM <= ResultSrcE;
             MemReadM <= MemReadE;
             MemWriteM <= MemWriteE;
-            RegWriteM <= RegWriteE; // Forward RegWrite signal
+            RegWriteM <= RegWriteE;
         end
     end
 
