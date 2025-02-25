391cc35dda2321f72f4693bedf23d9934656359e
matan-yemini <matany55@gmail.com>
2025-02-26 00:38:27 +0800

    Update forwardA.v comments

--- project_2.srcs/sources_1/new/forwardA.v
+++ project_2.srcs/sources_1/new/forwardA.v
@@ -1,11 +1,11 @@
 `timescale 1ns/1ps
 
 module forwardA(
-    input  [31:0] RD1E,        // ערך קריאה מהרגיסטר
-    input  [31:0] ResultW,     // ערך תוצאה משלב Write Back
-    input  [31:0] ALUResultM,  // ערך תוצאה משלב Memory
-    input  [1:0] ForwardA,     // סיגנל Forward מה-Hazard Unit
-    output [31:0] ForwardedA   // הפלט שנבחר
+    input  [31:0] RD1E,
+    input  [31:0] ResultW,
+    input  [31:0] ALUResultM,
+    input  [1:0] ForwardA,
+    output [31:0] ForwardedA
 );
 
     assign ForwardedA = (ForwardA == 2'b10) ? ALUResultM : 
